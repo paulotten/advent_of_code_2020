b9a75a3297f15573e5736ba908c330e263638a2c
Paul Otten <potten@my.bcit.ca>
2020-12-13 13:56:10 -0500

    Day 12 part 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,5 +1,7 @@
 mod data;
 
+use std::mem::swap;
+
 struct Direction {
     action: char,
     value: i32,
@@ -23,10 +25,95 @@ fn main() {
 
     let directions = data.lines().map(Direction::parse).collect();
 
-    puzzle1(directions);
+    puzzle2(directions);
 }
 
-fn puzzle1(directions: Vec<Direction>) {
+fn puzzle2(directions: Vec<Direction>) {
+    // waypoint
+    let mut x: i32 = 10;
+    let mut y: i32 = 1;
+    // ship
+    let mut ship_x: i32 = 0;
+    let mut ship_y: i32 = 0;
+
+    for direction in directions {
+        match direction.action {
+            'N' => y += direction.value,
+            'S' => y -= direction.value,
+            'E' => x += direction.value,
+            'W' => x -= direction.value,
+            'L' => {
+                let coord = left(x, y, direction.value as u32);
+                x = coord.0;
+                y = coord.1;
+            },
+            'R' => {
+                let coord = right(x, y, direction.value as u32);
+                x = coord.0;
+                y = coord.1;
+            },
+            'F' => {
+                ship_x += x * direction.value;
+                ship_y += y * direction.value;
+            },
+            a @ _ => panic!("Invalid action: {}", a),
+        }
+    }
+
+    println!("{}, {} ({})", ship_x, ship_y, ship_x.abs() + ship_y.abs());
+}
+
+fn right(x: i32, y: i32, degrees: u32) -> (i32, i32) {
+    rotate(x, y, degrees, true)
+}
+
+fn left(x: i32, y: i32, degrees: u32) -> (i32, i32) {
+    rotate(x, y, degrees, false)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_right() {
+        let mut coords = (10, 4);
+
+        coords = right(coords.0, coords.1, 90);
+        assert_eq!(coords, (4, -10));
+        coords = right(coords.0, coords.1, 90);
+        assert_eq!(coords, (-10, -4));
+        coords = right(coords.0, coords.1, 90);
+        assert_eq!(coords, (-4, 10));
+        coords = right(coords.0, coords.1, 90);
+        assert_eq!(coords, (10, 4));
+        
+        coords = right(coords.0, coords.1, 180);
+        assert_eq!(coords, (-10, -4));
+    }
+}
+
+fn rotate(mut x: i32, mut y: i32, mut degrees: u32, right: bool) -> (i32, i32) {
+    if degrees % 90 != 0 {
+        panic!("Degrees must be a multiple of 90, was {}", degrees);
+    }
+
+    while degrees > 0 {
+        if right {
+            x = -x;
+        } else {
+            y = -y;
+        }
+
+        swap(&mut x, &mut y);
+
+        degrees -= 90;
+    }
+
+    (x, y)
+}
+
+fn _puzzle1(directions: Vec<Direction>) {
     let mut x: i32 = 0;
     let mut y: i32 = 0;
     let mut current_direction = 'E';
@@ -37,8 +124,8 @@ fn puzzle1(directions: Vec<Direction>) {
             'S' => y -= direction.value,
             'E' => x += direction.value,
             'W' => x -= direction.value,
-            'L' => current_direction = turn(current_direction, -direction.value),
-            'R' => current_direction = turn(current_direction, direction.value),
+            'L' => current_direction = _turn(current_direction, -direction.value),
+            'R' => current_direction = _turn(current_direction, direction.value),
             'F' => match current_direction {
                 'N' => y += direction.value,
                 'S' => y -= direction.value,
@@ -53,7 +140,7 @@ fn puzzle1(directions: Vec<Direction>) {
     println!("{}, {} ({})", x, y, x.abs() + y.abs());
 }
 
-fn turn(starting_direction: char, mut degrees: i32) -> char {
+fn _turn(starting_direction: char, mut degrees: i32) -> char {
     let directions = ['N', 'E', 'S', 'W'];
     let i = directions.iter().position(|x| *x == starting_direction);
 
