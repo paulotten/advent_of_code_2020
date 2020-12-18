5bf1e2df5ad8f82179fa941df0048217377fb7f3
Paul Otten <potten@my.bcit.ca>
2020-12-17 21:12:41 -0500

    Day 17 part 2

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -7,6 +7,7 @@ struct Point {
     x: i32,
     y: i32,
     z: i32,
+    w: i32,
 }
 
 struct Bounds {
@@ -20,21 +21,15 @@ fn main() {
 
     let points = get_points(data);
 
-    puzzle1(points);
+    //_puzzle1(points);
+    puzzle2(points);
 }
 
-fn puzzle1(mut points: HashSet<Point>) {
+fn _puzzle1(mut points: HashSet<Point>) {
     let mut cycle = 0;
 
     loop {
         let bounds = get_bounds(&points);
-
-        /*
-        println!("Cycle {}", cycle);
-        println!();
-        _print_points(&points, &bounds);
-        */
-
         cycle += 1;
 
         if cycle > 6 {
@@ -46,7 +41,7 @@ fn puzzle1(mut points: HashSet<Point>) {
         for x in bounds.min.x-1 ..= bounds.max.x+1 {
             for y in bounds.min.y-1 ..= bounds.max.y+1 {
                 for z in bounds.min.z-1 ..= bounds.max.z+1 {
-                    let point = Point{x, y, z};
+                    let point = Point{x, y, z, w: 0};
                     let neighbors = get_neighbors(&point, &points);
 
                     if points.get(&point).is_none() {
@@ -68,20 +63,62 @@ fn puzzle1(mut points: HashSet<Point>) {
     println!("{}", points.len());
 }
 
+fn puzzle2(mut points: HashSet<Point>) {
+    let mut cycle = 0;
+
+    loop {
+        let bounds = get_bounds(&points);
+        cycle += 1;
+
+        if cycle > 6 {
+            break;
+        }
+
+        let mut new_points: HashSet<Point> = HashSet::new();
+
+        for x in bounds.min.x-1 ..= bounds.max.x+1 {
+            for y in bounds.min.y-1 ..= bounds.max.y+1 {
+                for z in bounds.min.z-1 ..= bounds.max.z+1 {
+                    for w in bounds.min.w-1 ..= bounds.max.w+1 {
+                        let point = Point{x, y, z, w};
+                        let neighbors = get_neighbors(&point, &points);
+
+                        if points.get(&point).is_none() {
+                            if neighbors == 3 {
+                                new_points.insert(point);
+                            }
+                        } else {
+                            if neighbors == 2 || neighbors == 3 {
+                                new_points.insert(point);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        points = new_points;
+    }
+
+    println!("{}", points.len());
+}
+
 fn get_neighbors(point: &Point, all_points: &HashSet<Point>) -> u8 {
     let mut count = 0;
 
     for x in point.x-1 ..= point.x+1 {
         for y in point.y-1 ..= point.y+1 {
             for z in point.z-1 ..= point.z+1 {
-                let test_point = Point{x, y, z};
+                for w in point.w-1 ..= point.w+1 {
+                    let test_point = Point{x, y, z, w};
 
-                if test_point == *point {
-                    continue;
-                }
+                    if test_point == *point {
+                        continue;
+                    }
 
-                if all_points.contains(&test_point) {
-                    count += 1;
+                    if all_points.contains(&test_point) {
+                        count += 1;
+                    }
                 }
             }
         }
@@ -90,27 +127,9 @@ fn get_neighbors(point: &Point, all_points: &HashSet<Point>) -> u8 {
     count
 }
 
-fn _print_points(points: &HashSet<Point>, bounds: &Bounds) {
-    for z in bounds.min.z..=bounds.max.z {
-        println!("z={}", z);
-
-        for y in bounds.min.y..=bounds.max.y {
-            for x in bounds.min.x..=bounds.max.x {
-                let c = match points.get(&Point{x, y, z}) {
-                    Some(_) => '#',
-                    None => '.',
-                };
-                print!("{}", c);
-            }
-            println!();
-        }
-        println!();
-    }
-}
-
 fn get_bounds(points: &HashSet<Point>) -> Bounds {
-    let mut min = Point{x: 0, y: 0, z: 0};
-    let mut max = Point{x: 0, y: 0, z: 0};
+    let mut min = Point{x: 0, y: 0, z: 0, w: 0};
+    let mut max = Point{x: 0, y: 0, z: 0, w: 0};
 
     let mut first = true;
 
@@ -119,10 +138,12 @@ fn get_bounds(points: &HashSet<Point>) -> Bounds {
             min.x = point.x;
             min.y = point.y;
             min.z = point.z;
+            min.w = point.w;
 
             max.x = point.x;
             max.y = point.y;
             max.z = point.z;
+            max.w = point.w;
 
             first = false;
         } else {
@@ -131,17 +152,23 @@ fn get_bounds(points: &HashSet<Point>) -> Bounds {
             } else if point.x > max.x {
                 max.x = point.x;
             }
-            
+
             if point.y < min.y {
                 min.y = point.y;
             } else if point.y > max.y {
                 max.y = point.y;
             }
-            
+
             if point.z < min.z {
                 min.z = point.z;
             } else if point.z > max.z {
                 max.z = point.z;
+            }
+
+            if point.w < min.w {
+                min.w = point.w;
+            } else if point.w > max.w {
+                max.w = point.w;
             }
         }
     }
@@ -152,7 +179,6 @@ fn get_bounds(points: &HashSet<Point>) -> Bounds {
 fn get_points(data: &str) -> HashSet<Point> {
     let mut points: HashSet<Point> = HashSet::new();
 
-    let z = 0;
     let lines: Vec<_> = data.lines().collect();
 
     for y in 0..lines.len() {
@@ -161,7 +187,7 @@ fn get_points(data: &str) -> HashSet<Point> {
         for x in 0..chars.len() {
             if chars[x] == '#' {
                 points.insert(Point{
-                    x: x as i32, y: y as i32, z
+                    x: x as i32, y: y as i32, z: 0, w: 0
                 });
             }
         }
