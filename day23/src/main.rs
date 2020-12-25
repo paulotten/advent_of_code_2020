987845e1564d71cea077d4414d32d9053e787c2b
Paul Otten <potten@my.bcit.ca>
2020-12-25 12:54:16 -0500

    Day 23 part 2

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,29 +1,37 @@
 fn main() {
     // puzzle data
     let data = "137826495";
-    let num_moves = 100;
+    //let num_moves = 100;
 
     // sample data
     //let data = "389125467";
     //let num_moves = 10;
 
-    let mut numbers: Vec<u8> = data.chars().map(|c| String::from(c).parse().unwrap()).collect();
+    let mut numbers: Vec<u32> = data.chars().map(|c| String::from(c).parse().unwrap()).collect();
+
+    // part 2
+    // ~7 hours to brute force
+    let num_moves = 10_000_000;
+
+    for i in numbers.len() as u32 + 1 ..= 1_000_000 {
+        numbers.push(i);
+    }
 
     part1(&mut numbers, num_moves);
 }
 
-fn part1(numbers: &mut Vec<u8>, num_moves: u8) {
-    let num_numbers = numbers.len() as u8;
+fn part1(numbers: &mut Vec<u32>, num_moves: u32) {
+    let num_numbers = numbers.len() as u32;
 
     for i in 1..=num_moves {
         println!("-- move {} --", i);
-        println!("cups: {:?}", numbers);
+        //println!("cups: {:?}", numbers);
 
         let current = numbers[0];
         //println!("currect: {}", current);
 
         let pickup = numbers[1..4].to_vec();
-        println!("pick up: {:?}", pickup);
+        //println!("pick up: {:?}", pickup);
 
         *numbers = numbers[4..].to_vec();
         //println!("remaining: {:?}", numbers);
@@ -40,7 +48,7 @@ fn part1(numbers: &mut Vec<u8>, num_moves: u8) {
 
             destination -= 1;
         }
-        println!("destination: {}", destination);
+        //println!("destination: {}", destination);
 
         let mut destination_index = numbers.iter().position(|x| *x == destination).unwrap();
 
@@ -52,8 +60,6 @@ fn part1(numbers: &mut Vec<u8>, num_moves: u8) {
 
         // add current to end
         numbers.push(current);
-
-        println!();
     }
 
     println!("-- final --");
