98047eaf1a587ee4a5ffee4507754bea7946463f
Paul Otten <potten@my.bcit.ca>
2020-12-28 10:39:03 -0500

    Day 25

---
+++ day25/src/main.rs
@@ -0,0 +1,51 @@
+fn main() {
+    let pub_a = 9033205;
+    let pub_b = 9281649;
+
+    // sample
+    //let pub_a = 5764801;
+    //let pub_b = 17807724;
+
+    part1(pub_a, pub_b);
+}
+
+fn part1(pub_a: u64, pub_b: u64) {
+    let mut value = 1;
+    let pub_x;
+    let mut loop_size = 0;
+
+    loop {
+        match value {
+            v if v == pub_a => {
+                pub_x = pub_b;
+                break;
+            },
+            v if v == pub_b => {
+                pub_x = pub_a;
+                break;
+            },
+            _ => {},
+        }
+
+        loop_size += 1;
+
+        value = crypto_loop(value, 7);
+    }
+
+    println!("{} {}", pub_x, loop_size);
+
+    value = 1;
+
+    for _ in 0..loop_size {
+        value = crypto_loop(value, pub_x);
+    }
+
+    println!("{}", value);
+}
+
+fn crypto_loop(mut value: u64, subject: u64) -> u64 {
+    value *= subject;
+    value %= 20201227;
+
+    value
+}
